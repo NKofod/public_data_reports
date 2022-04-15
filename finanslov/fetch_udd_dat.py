35868f05a89b6b966b2aba6bf335161e73df2f34
Nicolai Jacob Kofod-Jensen <nkof@itu.dk>
2022-04-15 22:53:19 +0200

    Put everything into two scripts

--- finanslov/fetch_udd_dat.py
+++ finanslov/fetch_udd_dat.py
@@ -2,6 +2,8 @@ import requests
 from bs4 import BeautifulSoup as Soup 
 import re 
 import pandas as pd 
+import os 
+
 universities = {"KU": "01", 
                 "AU": "05", 
                 "SDU": "11", 
@@ -11,8 +13,17 @@ universities = {"KU": "01",
                 "DTU": "37",
                 "ITU": "45"}
 
-for university in universities: 
+try: 
+    os.mkdir("data")
+    os.mkdir("data/raw")
+    os.mkdir("data/processed")
+except FileExistsError:
+    pass 
+except IsADirectoryError:
+    pass 
 
+
+for university in universities: 
     urls = ["22-1","18","13A", "08A"]
     for url in urls:
         print(f"Working on {url}")
@@ -32,38 +43,12 @@ for university in universities:
             elif "Virksomhedsoversigt" in i: 
                 virk_num = tmp_list.index(i)
         
-        optag = tmp_list[optage_num:virk_num]
 
         virk = tmp_list[virk_num:]  
-        optag = [i.split("|")[1:-1] for i in optag if "|" in i]
         virk = [i.split("|")[1:-1] for i in virk if "|" in i]
 
-        optag[0][0] = "Finanslov"
-        optag[1][0] = "År"
         virk[0][0] = "Finanslov"
         virk[1][0] = "År"
-        for i in range(len(optag)):
-            for j in range(len(optag[i])):
-                optag[i][j] = optag[i][j].replace(".","")
-                optag[i][j] = re.sub("^\s+(.+?)$",r"\1",optag[i][j])
-                optag[i][j] = re.sub("\s+$",r"",optag[i][j])
-                if optag[i][j] == "-":
-                    optag[i][j] == None 
-                # optag[i][j] = optag[i][j].replace(" ","")
-                try: 
-                    optag[i][j] = int(optag[i][j])
-                except ValueError:
-                    pass 
-                except TypeError:
-                    pass 
-                if optag[i][j] == "achelor":
-                    optag[i][j] = "Bachelor"
-                elif optag[i][j] == "andidat":
-                    optag[i][j] = "Kandidat"
-                elif optag[i][j] == "alt":
-                    optag[i][j] = "Total"
-            if optag[i][1:] == [" "] * (len(optag[i]) -1): 
-                optag[i+1][0] = optag[i][0] + " " + optag[i+1][0]
         act_num = 0 
 
         for i in range(len(virk)):
@@ -92,31 +77,13 @@ for university in universities:
                 if virk[i][j] == "alt omsætning (mio kr)": 
                     virk[i][j] = "Total omsætning (mio kr)"
                 
-        aktivitet = virk[act_num+1:]
         virk = virk[:act_num]
-
         virk_dict = {}
-        optag_dict = {}
-        aktivitet_dict = {}
-
         for i in virk: 
             if i[1:] == [""] * len(i[1:]):
                 continue 
             virk_dict[i[0]] = i[1:]
-        for i in optag:
-            if i[1:] == [""] * len(i[1:]):
-                continue 
-            optag_dict[i[0]] = i[1:]
-        for i in aktivitet:
-            if i[1:] == [""] * len(i[1:]):
-                continue 
-            aktivitet_dict[i[0]] = i[1:]
 
         virk_df = pd.DataFrame(virk_dict)
-        # optag_df = pd.DataFrame(optag_dict)
-        # aktivitet_df = pd.DataFrame(aktivitet_dict)
-        # aktivitet_df['År'] = virk_df['År']
-        # aktivitet_df['Finanslov'] = virk_df['Finanslov']
-        virk_df.to_csv(f"{university}_virk_{url}.csv",index=False,sep=";")
-        # optag_df.to_csv(f"{university}_optag_{url}.csv",index=False,sep=";")
-        # aktivitet_df.to_csv(f"{university}_aktivitet_{url}.csv",index=False,sep=";")
+
+        virk_df.to_csv(f"./data/raw/{university}_virk_{url}.csv",index=False,sep=";")
